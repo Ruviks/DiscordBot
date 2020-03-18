936e205c81ca88293802b1896eca608415c0ee65
Deadlock <poghosyan26@gmail.com>
2020-03-18 17:41:33 +0400

    basic command carcass setup

--- boot.js
+++ boot.js
@@ -1,18 +1,26 @@
 const config = require('config');
-
 const discordConfig = config.get('discord')
 const Discord = require('discord.js');
-let token = process.env.DISCORD_BOT_TOKEN || discordConfig.api_key
-const client = new Discord.Client();
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+const client = new CommandoClient({
+    commandPrefix: '?',
+    owner: process.env.OWNER_ID || null,
+    invite: 'https://discord.gg/FkxwyzS',
 });
+const token = process.env.DISCORD_BOT_TOKEN || discordConfig.api_key;
 
-client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
-    }
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ['misc', 'miscellaneous'],
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands({ help: true, ping: true })
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+    client.user.setActivity('with Commando');
 });
-
+client.on('error', console.error);
 client.login(token);
