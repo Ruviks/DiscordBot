483ad209e55992120e1c15732b22027a2d3f61a8
Deadlock <poghosyan26@gmail.com>
2020-03-18 16:38:59 +0400

    basic ping command

--- boot.js
+++ boot.js
@@ -1,6 +1,18 @@
-const config =  require('config');
+const config = require('config');
 
 const discordConfig = config.get('discord')
+const Discord = require('discord.js');
+let token = process.env.DISCORD_BOT_TOKEN || discordConfig.api_key
+const client = new Discord.Client();
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
-console.log(process.env.DISCORD_BOT_TOKEN || discordConfig.api_key)
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+});
+
+client.login(token);
