c144f803e6ab498bec375641af8a67e8d32aa31e
Ruviks <poghosyan26@gmail.com>
2021-07-05 19:37:39 +0400

    fix path

--- src/types/avaliableGames.js
+++ src/types/avaliableGames.js
@@ -1,3 +1,3 @@
 const fs = require('fs');
-const avaliableGames = JSON.parse(fs.readFileSync("C:/Users/Ruvik/Desktop/DiscordBot/src/types/gamedata.json"))
+const avaliableGames = JSON.parse(fs.readFileSync("src/types/gamedata.json"))
 module.exports = avaliableGames
